7e8cd523727016308ca640f8a056436cdc0d553a
Mario Velazquez <mario.velazquez1@uoit.net>
2023-08-29 23:10:40 -0400

    Progress with Webcam

--- LearningJavaScript/19 - Webcam Fun/scripts.js
+++ LearningJavaScript/19 - Webcam Fun/scripts.js
@@ -3,3 +3,45 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+        })
+        .catch(err=> {
+            console.error("OH no", err);
+        });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    setInterval(()=> {
+        ctx.drawImage(video, 0 , 0, width, height);
+    }, 16);
+}
+
+function takePhoto() {
+    // playted sound
+    snap.currentTime = 0;
+    snap.play();
+
+    //Take data out
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.textContent = "Download Image";
+    strip.insertBefore(link, strip.firstChild);
+
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
