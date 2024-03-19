29de8642b1dabb46c61bf1894ccfca9beb379c5f
Mario Velazquez <mario.velazquez1@uoit.net>
2024-03-18 20:31:43 -0400

    ReactFacts Site Complete!

---
+++ LearningReact/LearnReact/vite-react/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Navbar(){
+
+    return (
+        <nav className="navbar">
+            <img src="../src/assets/react-icon-small.png" className="navbar-img"></img>
+            <h3 className="navbar-react">
+                ReactFacts
+            </h3>
+            <h4 className="navbar-course">
+                React Course - Project 1
+            </h4>
+        </nav>
+    )
+}
